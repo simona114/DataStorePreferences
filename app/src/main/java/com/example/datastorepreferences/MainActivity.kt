0c31b11fc527709714df2724d5e20d99552ae34e
Simona Nikolova <57250076+simona114@users.noreply.github.com>
2023-03-27 11:19:36 +0300

    Save data and read it from DataStore preferences

---
+++ app/src/main/java/com/example/datastorepreferences/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.datastorepreferences
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.lifecycleScope
+import com.example.datastorepreferences.databinding.ActivityMainBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore("settings")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSubmit.setOnClickListener {
+            lifecycleScope.launch {
+                saveDataToDataStore(
+                    key = binding.tvKey.text.toString(),
+                    value = binding.tvValue.text.toString()
+                )
+            }
+        }
+
+        binding.btnGetValueForKey.setOnClickListener {
+            lifecycleScope.launch {
+                binding.tvValueForKey.text =
+                    readDataFromDataStore(key = binding.tvKeyForValue.text.toString()) ?: "no value for this key"
+            }
+
+        }
+    }
+
+    private suspend fun saveDataToDataStore(key: String, value: String) {
+        val dataStoreKey = stringPreferencesKey(key)
+        dataStore.edit { settings ->
+            settings[dataStoreKey] = value
+        }
+    }
+
+    private suspend fun readDataFromDataStore(key: String): String? {
+        val dataStoreKey = stringPreferencesKey(key)
+        val preferences = dataStore.data.first()
+        return preferences[dataStoreKey]
+    }
+}
